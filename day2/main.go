c4158a34f998df72483660614e2fb8bbc6ee8052
Guilherme Martins <gui.martins.soares@gmail.com>
2024-05-15 19:37:59 -0300

    example showing communication between threads via channel

--- day2/main.go
+++ day2/main.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-func contador(x int) {
+func counter(x int) {
 	for i := 0; i < x; i++ {
 		fmt.Println(i)
 		time.Sleep(time.Second)
@@ -13,8 +13,16 @@ func contador(x int) {
 }
 
 func main() {
-	//Using go routines we can easily run these functions simultaneously
-	go contador(10)
-	go contador(10)
-	contador(10)
+	/*Using go routines we can easily run functions simultaneously.
+	We create channels so that we can communicate between threads allocated in different memory addresses
+	Sharing memory addresses is not recommended since one function can overwrite data that is being used by
+	another function, rendering it useless.*/
+	channel := make(chan string)
+
+	go func() {
+		channel <- "hey!"
+	}()
+
+	message := <-channel
+	fmt.Println(message)
 }
