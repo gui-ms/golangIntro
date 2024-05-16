1f5e824d4a55f7f307700c2602ba1202eea57809
Guilherme Martins <gui.martins.soares@gmail.com>
2024-05-16 00:31:10 -0300

    Created a simple load balancer

--- day2/main.go
+++ day2/main.go
@@ -5,24 +5,30 @@ import (
 	"time"
 )
 
-func counter(x int) {
-	for i := 0; i < x; i++ {
-		fmt.Println(i)
+func worker(workerId int, data chan int) {
+	for x := range data {
+		fmt.Printf("Worker %d received %d \n", workerId, x)
 		time.Sleep(time.Second)
 	}
 }
 
+/*Using go routines we can easily run functions simultaneously.
+We create channels so that we can communicate between threads allocated in different memory addresses
+Sharing memory addresses is not recommended since one function can overwrite data that is being used by
+another function, rendering it useless.*/
+
 func main() {
-	/*Using go routines we can easily run functions simultaneously.
-	We create channels so that we can communicate between threads allocated in different memory addresses
-	Sharing memory addresses is not recommended since one function can overwrite data that is being used by
-	another function, rendering it useless.*/
-	channel := make(chan string)
+	channel := make(chan int)
 
-	go func() {
-		channel <- "hey!"
-	}()
+	//Here we created a load balancer with ease
 
-	message := <-channel
-	fmt.Println(message)
+	workersAmount := 10
+
+	for j := range workersAmount {
+		go worker(j, channel)
+	}
+
+	for i := range 50 {
+		channel <- i
+	}
 }
